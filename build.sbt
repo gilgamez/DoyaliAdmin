daa9d4f319f7a1044e81d8334b4343128921665f
Gilgamesh Nootebos <gilgamez@gmail.com>
2013-02-27 00:14:23 +0100

    read metadata and put json out

--- build.sbt
+++ build.sbt
@@ -13,3 +13,9 @@ libraryDependencies += "io.spray" % "spray-can" % "1.1-M7"
 libraryDependencies += "io.spray" %  "spray-json_2.10" % "1.2.3"
 
 libraryDependencies += "com.typesafe.akka" % "akka-actor_2.10" % "2.1.0"
+
+libraryDependencies += "org.json4s" %% "json4s-native" % "3.1.0"
+
+libraryDependencies += "com.drewnoakes" % "metadata-extractor" % "2.6.2"
+
+libraryDependencies += "com.github.scala-incubator.io" %% "scala-io-core" % "0.4.2"
--- src/main/scala/com/doyali/spray1/Main.scala
+++ src/main/scala/com/doyali/spray1/Main.scala
@@ -1,8 +1,19 @@
 package com.doyali.spray1
 
 import spray.routing.SimpleRoutingApp
+import org.json4s.native.JsonMethods._
+import org.json4s.JsonDSL.WithBigDecimal._
+import com.drew.imaging.ImageMetadataReader
+import java.io.BufferedInputStream
+import scala.collection.JavaConversions._
+import org.json4s._
+import scala.Some
+import com.drew.metadata.Tag
+
 
 object Main extends App with SimpleRoutingApp {
+
+
   startServer(interface = "localhost", port = 8080) {
     path("hello") {
       get {
@@ -14,17 +25,22 @@ object Main extends App with SimpleRoutingApp {
     pathPrefix("photo") {
       path("all") {
         complete {
-          <ul>
-            <li>IMG_31001.jpg</li>
-          </ul>
+          pretty(render(List("IMG_4847.JPG", "foto 1-1.JPG")))
         }
 
       } ~
-      path("id" / PathElement) { id =>
+      path("id" / PathElement) {
+        id =>
           complete {
-            "{" + id + "}"
+            implicit def tag2jvalue(x: Tag) = JObject(x.getTagName().toString,JString(x.getDescription()))
+
+            val result = Some(id)
+            import scalax.io.Resource._
+            val reader = fromClasspath(id).map { is =>  ImageMetadataReader.readMetadata(new BufferedInputStream(is), true) }
+            render( reader.now.getDirectories().flatMap( d => d.getTags()) )
+            //               pretty(render(result))
           }
-        }
       }
+    }
   }
 }
