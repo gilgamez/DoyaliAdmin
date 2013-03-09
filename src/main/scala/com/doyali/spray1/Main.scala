c3e7fe38f8686d8ba7648ed9644605e83d2a06f0
Gilgamesh Nootebos <gilgamez@gmail.com>
2013-03-09 21:12:49 +0100

    read metadata and put json out

--- src/main/scala/com/doyali/spray1/Main.scala
+++ src/main/scala/com/doyali/spray1/Main.scala
@@ -7,8 +7,7 @@ import com.drew.imaging.ImageMetadataReader
 import java.io.BufferedInputStream
 import scala.collection.JavaConversions._
 import org.json4s._
-import scala.Some
-import com.drew.metadata.Tag
+import com.drew.metadata.{Directory, Tag}
 
 
 object Main extends App with SimpleRoutingApp {
@@ -32,12 +31,21 @@ object Main extends App with SimpleRoutingApp {
       path("id" / PathElement) {
         id =>
           complete {
-            implicit def tag2jvalue(x: Tag) = JObject(x.getTagName().toString,JString(x.getDescription()))
+            import org.json4s.JsonAST._
 
-            val result = Some(id)
+            implicit def tag2jvalue(x: Tag) = JObject(
+              JField("name",x.getTagName)::
+              JField("description",x.getDescription) ::
+              JField("",x.)
+              Nil)
+            //            implicit def tag2jvalue(x: Tag) = JObject(List({"name" -> x.getTagName();"description"->x.getDescription()))
+            implicit def directory2json(in: Directory) = JArray(in.getTags.map( tag2jvalue(_)))
+
             import scalax.io.Resource._
             val reader = fromClasspath(id).map { is =>  ImageMetadataReader.readMetadata(new BufferedInputStream(is), true) }
-            render( reader.now.getDirectories().flatMap( d => d.getTags()) )
+
+            val result  = reader.map( _.getDirectories.flatMap(d => d.getTags).map(tag2jvalue(_)))
+            pretty(render(result.opt))
             //               pretty(render(result))
           }
       }
